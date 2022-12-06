c60b09300cc9253b4f31ee10f209dd00211705a8
sakshamrana7 <rana.saksham10@gmail.com>
2022-12-05 20:42:12 -0800

    final changes

--- src/PasswordUI.java
+++ src/PasswordUI.java
@@ -6,9 +6,18 @@ public class PasswordUI extends JDialog implements ActionListener {
     boolean verified, editing;
     JTextField input;
     String password;
-    public PasswordUI(String _password, boolean _editing) {
-        super(null, _editing ? "Change password" : "Credential Check", ModalityType.DOCUMENT_MODAL);   // set modality so the main thread in InventorySystem that calls this constructor waits until this dialog gets disposed
 
+    public PasswordUI(String _password, boolean _editing) {
+        super(null, _editing ? "Change password" : "Credential Check", ModalityType.DOCUMENT_MODAL); // set modality so
+                                                                                                     // the main thread
+                                                                                                     // in
+                                                                                                     // InventorySystem
+                                                                                                     // that calls this
+                                                                                                     // constructor
+                                                                                                     // waits until this
+                                                                                                     // dialog gets
+                                                                                                     // disposed
+        String lang = MainUI.selected_lang;
         verified = false;
         editing = _editing;
         password = _password;
@@ -23,29 +32,32 @@ public class PasswordUI extends JDialog implements ActionListener {
         submitButton.addActionListener(this);
 
         Insets dummy = new Insets(0, 0, 0, 0);
-        root.add(input, new GridBagConstraints(0, 0, 1, 1, 0.9, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, dummy, 0, 0));
-        root.add(submitButton, new GridBagConstraints(1, 0, 1, 1, 0.1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, dummy, 0, 0));
+        root.add(input, new GridBagConstraints(0, 0, 1, 1, 0.9, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                dummy, 0, 0));
+        root.add(submitButton, new GridBagConstraints(1, 0, 1, 1, 0.1, 1, GridBagConstraints.CENTER,
+                GridBagConstraints.BOTH, dummy, 0, 0));
 
         // frame parameter boilerplate code
-        this.add(root);    // add the main root panel to the frame
+        this.add(root); // add the main root panel to the frame
         this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        //this.setTitle("Credential Check");    // this line commented out as the code for modality already sets the title
+        // this.setTitle("Credential Check"); // this line commented out as the code for
+        // modality already sets the title
         this.pack();
         this.setSize(300, 90);
         this.setLocation(500, 300);
-        this.setVisible(true);     // actually show the window
+        this.setVisible(true); // actually show the window
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {    // this is a listener method that waits for any GUI event to occur and runs the corresponding method for each event
+    public void actionPerformed(ActionEvent e) { // this is a listener method that waits for any GUI event to occur and
+                                                 // runs the corresponding method for each event
         if (e.getActionCommand().equals("submit")) {
             if (editing) {
                 password = input.getText();
                 if (password.equals(""))
                     return;
                 JOptionPane.showMessageDialog(this, "Password set to '" + password + "'");
-            }
-            else {
+            } else {
                 if (input.getText().equals(password))
                     verified = true;
                 else {
